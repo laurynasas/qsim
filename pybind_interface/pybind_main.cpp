1393616cfde45205b95534024a058fd34c9f27fa
Laurynas Tamulevicius <laurynast@google.com>
2021-06-16 13:02:57 +0300

    enable test workflow on custom branches

--- pybind_interface/pybind_main.cpp
+++ pybind_interface/pybind_main.cpp
@@ -618,7 +618,7 @@ class SimulatorHelper {
     state_space.NormalToInternalOrder(state);
   }
 
-  Runner::Parameter get_params() const {
+  typename Runner::Parameter get_params() const {
     typename Runner::Parameter params;
     params.num_threads = num_threads;
     params.max_fused_size = max_fused_size;
@@ -627,7 +627,7 @@ class SimulatorHelper {
     return params;
   }
 
-  Runner::Parameter get_noisy_params() const {
+  typename Runner::Parameter get_noisy_params() const {
     typename NoisyRunner::Parameter params;
     params.max_fused_size = max_fused_size;
     params.verbosity = verbosity;
