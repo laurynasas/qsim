95209f09ddc0df1f60cf0010a2727a7aa536d87e
Laurynas Tamulevicius <laurynast@google.com>
2021-07-13 23:39:40 +0100

    dev

--- pybind_interface/decide/decide.cpp
+++ pybind_interface/decide/decide.cpp
@@ -10,7 +10,7 @@ namespace py = pybind11;
 #else
 //  GCC Intrinsics
 #include <cpuid.h>
-void cpuid(unsigned info[4], int infoType){
+void cpuid(int info[4], int infoType){
     __cpuid_count(infoType, 0, info[0], info[1], info[2], info[3]);
 }
 
@@ -20,23 +20,23 @@ enum Instructions { AVX512F = 0, AVX2 = 1, SSE4_1 = 2, BASIC = 3};
 
 int detect_instructions() {
   Instructions instr = BASIC;
-  unsigned info[4];
+  int info[4];
 
   cpuid(info, 0);
 
   int nIds = info[0];
   if (nIds >= 1) {
     cpuid(info, 1);
-    if ((info[2] & ((unsigned)1 << 19)) != 0) {
+    if ((info[2] & (1 << 19)) != 0) {
       instr = SSE4_1;
     }
   }
   if (nIds >= 7) {
     cpuid(info, 7);
-    if ((info[1] & ((unsigned)1 <<  5) )!= 0) {
+    if ((info[1] & (1 <<  5) )!= 0) {
       instr = AVX2;
     }
-    if ((info[1] & ((unsigned)1 << 16)) != 0) {
+    if ((info[1] & (1 << 16)) != 0) {
       instr = AVX512F;
     }
 
