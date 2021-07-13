262f017ade6789344cf4e812c7070b35869ef773
Laurynas Tamulevicius <laurynast@google.com>
2021-07-13 20:07:58 +0100

    dev

--- pybind_interface/decide/decide.cpp
+++ pybind_interface/decide/decide.cpp
@@ -1,9 +1,6 @@
 #include <pybind11/pybind11.h>
-#include <iostream>
-#include <fstream>
 
 namespace py = pybind11;
-using namespace std;
 
 #ifdef _WIN32
 //  Windows
@@ -25,54 +22,25 @@ int detect_instructions() {
   Instructions instr = BASIC;
   unsigned info[4];
 
-  ofstream myfile;
-  myfile.open("examplee.txt", std::ios_base::app);
-
-  myfile << "## decide cpp entry: \n";
-
-  for (int i = 4 - 1; i >= 0; i--)
-    myfile <<i<<"-th element =" << info[i] <<"\n";
-
   cpuid(info, 0);
-  myfile << "## 1: \n";
-  for (int i = 4 - 1; i >= 0; i--)
-  myfile <<i<<"-th element =" << info[i] <<"\n";
 
   int nIds = info[0];
   if (nIds >= 1) {
     cpuid(info, 1);
     if ((info[2] & ((unsigned)1 << 19)) != 0) {
-      myfile << "supportrs sse41: \n";
       instr = SSE4_1;
     }
   }
-  myfile << "## 2: \n";
-  for (int i = 4 - 1; i >= 0; i--)
-  myfile <<i<<"-th element =" << info[i] <<"\n";
   if (nIds >= 7) {
     cpuid(info, 7);
     if ((info[1] & ((unsigned)1 <<  5) )!= 0) {
-      myfile << "supportrs avx2: \n";
       instr = AVX2;
     }
-    myfile << "## --------------: \n";
-    unsigned hh = (unsigned)1 << 16;
-    unsigned res = info[1] & ((unsigned)1 << 16);
-    bool bres = (res != 0);
-    myfile << info[1] <<"&" << hh << "=" << res << std::boolalpha << bres <<"\n";
-    myfile << typeid(res).name() << " | " << typeid(((unsigned)0)).name() << "\n";
-    myfile << std::boolalpha << ((info[1] & ((unsigned)1 << 16)) != ((unsigned)0)) <<"\n";
     if ((info[1] & ((unsigned)1 << 16)) != 0) {
-      myfile << "supportrs avx512: \n";
       instr = AVX512F;
     }
-    myfile << "## --------------: \n";
 
   }
-  myfile << "## 3: \n";
-  for (int i = 4 - 1; i >= 0; i--)
-  myfile <<i<<"-th element =" << info[i] <<"\n";
-  myfile.close();
 
   return static_cast<int>(instr);
 }
