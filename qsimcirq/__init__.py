a95a66b4a9a21d2b2cfc4eb6f47d4dbefe761975
Laurynas Tamulevicius <laurynast@google.com>
2021-07-13 20:18:13 +0100

    dev

--- qsimcirq/__init__.py
+++ qsimcirq/__init__.py
@@ -3,21 +3,15 @@ from qsimcirq import qsim_decide
 
 
 def _load_simd_qsim():
-    with open("build_log", 'a+') as f:
-        f.write("----> entry")
-        instr = qsim_decide.detect_instructions()
-        if instr == 0:
-            f.write("----> hsim 0")
-            qsim = importlib.import_module("qsimcirq.qsim_avx512")
-        elif instr == 1:
-            f.write("----> hsim 1")
-            qsim = importlib.import_module("qsimcirq.qsim_avx2")
-        elif instr == 2:
-            f.write("----> hsim 2")
-            qsim = importlib.import_module("qsimcirq.qsim_sse")
-        else:
-            f.write("----> hsim 3")
-            qsim = importlib.import_module("qsimcirq.qsim_basic")
+    instr = qsim_decide.detect_instructions()
+    if instr == 0:
+        qsim = importlib.import_module("qsimcirq.qsim_avx512")
+    elif instr == 1:
+        qsim = importlib.import_module("qsimcirq.qsim_avx2")
+    elif instr == 2:
+        qsim = importlib.import_module("qsimcirq.qsim_sse")
+    else:
+        qsim = importlib.import_module("qsimcirq.qsim_basic")
     return qsim
 
 
